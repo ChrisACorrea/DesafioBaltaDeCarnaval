2cd2836a3263fbf50499dff01457156cb1c66a23
Christopher Corrêa <christopher.a.c@outlook.com>
2023-02-17 11:09:57 -0300

    first commit

---
+++ Desafio01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite um texto:");
+Console.WriteLine("");
+
+var text = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("");
+Console.WriteLine($"> {CountCharacters(text)} caracteres, {CountWords(text)} palavras");
+
+
+int CountCharacters(string text)
+{
+	return text.Length;
+}
+
+int CountWords(string text)
+{
+	var cleanText = Regex.Replace(text.Trim(), "\\s+", " ");
+	if (cleanText.Length is not 0)
+		return cleanText.Split(" ").Length;
+
+	return 0;
+}
