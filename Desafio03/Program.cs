382d181f6554e822cb35f6b5011e47680f6d4698
Christopher Corrêa <christopher.a.c@outlook.com>
2023-02-19 18:34:37 -0300

    Adicionado Desafio 03

---
+++ Desafio03/Program.cs
@@ -0,0 +1,37 @@
+﻿var alcoholPrice = GetValueFromConsole("Informe o preço do Álcool: R$ ");
+var gasolinePrice = GetValueFromConsole("Informe o preço da Gasolina: R$ ");
+
+var priceRatio = alcoholPrice / gasolinePrice;
+
+Console.Clear();
+Console.WriteLine($"Preço do Álcool: {alcoholPrice:c2}");
+Console.WriteLine($"Preço da Gasolina: {gasolinePrice:c2}");
+Console.WriteLine($"Relação: {priceRatio:P}");
+Console.WriteLine("");
+
+if (priceRatio <= 0.72m)
+	Console.WriteLine("Compensa abastecer com álcool.");
+else
+	Console.WriteLine("Compensa abastecer com gasolina.");
+
+Console.WriteLine("");
+
+decimal GetValueFromConsole(string displayText)
+{
+	bool success;
+	decimal value;
+	do
+	{
+		Console.Write(displayText);
+		success = decimal.TryParse(Console.ReadLine(), out value);
+		if (!success)
+		{
+			Console.WriteLine("");
+			Console.Write("Você passou um valor inválido. Pressione qualquer tecla para tentar novamente");
+			Console.ReadKey();
+			Console.Clear();
+		}
+	} while (!success);
+
+	return value;
+}
