d41e62b25f1919ea91543c032028736b9131f6ae
Christopher Corrêa <christopher.a.c@outlook.com>
2023-02-20 15:25:47 -0300

    Adicionado Desafio 04

---
+++ Desafio04/Program.cs
@@ -0,0 +1,52 @@
+﻿using Desafio04;
+
+var finalPurchaseValue = ConsoleHelper.GetNumberValueFromConsole("Valor final da compra: R$ ");
+var paymentAmount = ConsoleHelper.GetNumberValueFromConsole("Pagamento: R$ ");
+var changeValue = paymentAmount - finalPurchaseValue;
+
+Console.WriteLine("");
+
+if (changeValue < 0)
+{
+	Console.WriteLine("O valor do pagamento é menor que o valor final da compra.");
+	Console.WriteLine($"Nesse caso não há troco e ainda permanece uma dívida de {(changeValue * -1):c2}.");
+	Console.WriteLine("");
+	Environment.Exit(0);
+}
+
+decimal[] values = { 200, 100, 50, 20, 10, 5, 2, 1, 0.50m, 0.25m, 0.10m, 0.05m, 0.01m };
+var coinQuantities = new Dictionary<decimal, int>();
+
+foreach (var value in values)
+	coinQuantities.Add(value, 0);
+
+CalculateChange(changeValue, 0);
+
+Console.WriteLine($"Troco:\t{changeValue:c2}");
+
+foreach (var coin in coinQuantities.Where((v) => v.Value != 0))
+{
+	if (coin.Key > 1)
+		Console.WriteLine($"\t- {coin.Value} nota{Plural(coin.Value)} de {coin.Key:n0} reais.");
+	else
+		Console.WriteLine($"\t- {coin.Value} moeda{Plural(coin.Value)} de {(coin.Key * 100):n0} centavos.");
+}
+
+string Plural(int quantity) => quantity > 1 ? "s" : string.Empty;
+
+void CalculateChange(decimal changeValue, int index)
+{
+
+	if (index == values.Length)
+		return;
+
+	var currentValue = values[index];
+
+	while (changeValue >= currentValue)
+	{
+		changeValue -= currentValue;
+		coinQuantities![currentValue]++;
+	}
+
+	CalculateChange(changeValue, index + 1);
+}
---
+++ Desafio04/README.md
@@ -0,0 +1,13 @@
+# DESAFIO 04 - 20/FEV
+
+**Objetivo:**  
+Escreva um programa que informa o troco a ser recebido pelo cliente, por exemplo:
+
+```text
+- Valor final da compra: R$ 328,00
+- Pagamento: R$ 400,00
+- Troco:
+	- 1 nota de 2 reais
+	- 2 notas de 10 reais
+	- 2 notas de 50 reais
+```
