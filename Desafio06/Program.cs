8d3d9e5e58df3b40dcde5f646d535a536d72c660
Christopher Corrêa <christopher.a.c@outlook.com>
2023-02-22 12:13:30 -0300

    Adicionado Desafio 06

---
+++ Desafio06/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("");
+Console.WriteLine("Texto a ser criptografado:");
+var text = Console.ReadLine()?.Trim().ToLower();
+
+if (string.IsNullOrEmpty(text))
+{
+	Console.WriteLine("Nenhum texto para encriptar.");
+	Environment.Exit(0);
+}
+
+var encryptedText = ApplyCaesarCipher(text!);
+
+Console.WriteLine("");
+Console.WriteLine(encryptedText.ToUpper());
+Console.WriteLine("");
+
+string ApplyCaesarCipher(string text)
+{
+	const int STEP = 3;
+	const int RANGE = 26;
+	const int AlPHABET_START = 'a';
+	string encryptedText = string.Empty;
+
+	foreach (var letter in text)
+	{
+		if (!char.IsAsciiLetter(letter))
+		{
+			encryptedText += letter;
+			continue;
+		}
+
+		var encryptedLetter = (char)(((letter - AlPHABET_START + STEP) % RANGE) + AlPHABET_START);
+		encryptedText += encryptedLetter;
+	}
+
+	return encryptedText;
+}
---
+++ Desafio06/README.md
@@ -0,0 +1,13 @@
+# DESAFIO 06 - 22/FEV
+
+**Objetivo:**  
+Escreva um programa que encripta um texto utilizando uma tabela de mapeamento de caracteres, por exemplo:
+
+a = K, n = J, d = A, r = T, e = L
+  
+**Resultado esperado:**
+  
+```text
+Input: andre
+Output: KJATE
+```
